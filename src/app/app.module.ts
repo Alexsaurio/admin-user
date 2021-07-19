23a451be7901ecf19f96819f444c65d9866684db
unknown <alex-ar@outlook.com>
2021-07-19 15:04:06 -0500

    creacion de componentes  base layout home y login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// importacion de servicios
+import { UserService  } from './core/services/user.service';
+
+// importacion del layout
+import { LoginComponent } from './layout/login/login.component';
+import { HomeComponent } from './layout/home/home.component';
+
+//importacion de componentes
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
