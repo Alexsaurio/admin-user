1cf3c2b6335eeb824cb8c2c03a0b2bb7850ff91f
unknown <alex-ar@outlook.com>
2021-07-20 16:56:51 -0500

    base de sidebar para mostrar informacion del usuario

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
+
+// importacion modelos
+import { User } from 'src/app/core/models/user.model';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +12,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  userView: User = {
+    "id": 2,
+    "email": "janet.weaver@reqres.in",
+    "first_name": "Janet",
+    "last_name": "Weaver",
+    "avatar": "https://reqres.in/img/faces/2-image.jpg"
+  }
 
   constructor() { }
 
