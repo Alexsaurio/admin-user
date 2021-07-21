06d15b194bf5a76c27bb70ffc41ed3e614aa04b0
unknown <alex-ar@outlook.com>
2021-07-20 23:17:42 -0500

    metodo para actualizar la informacion de un usuario seleccionado

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -2,9 +2,14 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { MatSidenav } from '@angular/material/sidenav';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 // importacion modelos
 import { User } from 'src/app/core/models/user.model';
+
+// importacion de servicios
+import { UserService } from 'src/app/core/services/user/user.service';
+import { AlertService } from 'src/app/core/services/alert/alert.service';
 
 @Component({
   selector: 'app-home',
@@ -16,18 +21,59 @@ export class HomeComponent implements OnInit {
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
 
-  userView: User;
+  userView: User = {
+    id: 1,
+    email: '',
+    first_name: '',
+    last_name: '',
+    avatar: ''
+  };
 
-  constructor() { }
+  isEdit: Boolean = false;
+
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private userservice: UserService, private as: AlertService) { }
 
   ngOnInit(): void {
-    this.userView = {
-      id: 2,
-      email: "janet.weaver@reqres.in",
-      first_name: "Janet",
-      last_name: "Weaver",
-      avatar: "https://reqres.in/img/faces/2-image.jpg"
-    }
+    this.getUserDetail(2);
   }
 
+  getUserDetail(userId: number){
+    this.userservice.getUserDetail(userId).subscribe(
+      response => {
+        this.userView = response.data;
+        this.newFormUser(response.data.first_name + " " + response.data.last_name, '')
+      },
+      error => {
+        this.as.alertError(error.message);
+      }
+    )
+  }
+
+  newFormUser(name: string, job: string){
+    this.userForm = this.fb.group
+    ({
+      name: new FormControl(name),
+      job: new FormControl(job),
+    });
+  }
+
+  changeEdit(){
+    this.isEdit = !this.isEdit;
+  }
+
+  updateUser(){
+    this.userservice.putUserDetail(this.userView.id, this.userForm.value).subscribe(
+      response => {
+        this.newFormUser(response.name, response.job);
+        this.userView.first_name = response.name;
+        this.changeEdit();
+        this.as.alertSuccess("Update user name: " + response.name + " job: " + response.job + " "  + response.updatedAt)
+      },
+      error => {
+        this.as.alertError(error.error.error);
+      }
+    )
+  }
 }
