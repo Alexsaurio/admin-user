29b46a41be435b32785f432903009f1e7946fe9c
unknown <alex-ar@outlook.com>
2021-07-22 09:18:30 -0500

    base test unitario RegisterComponent

--- src/app/layout/home/home.component.spec.ts
+++ src/app/layout/home/home.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +12,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [MatSidenavModule, BrowserAnimationsModule, HttpClientModule, RouterTestingModule, FormsModule, ReactiveFormsModule],
     })
     .compileComponents();
   }));
